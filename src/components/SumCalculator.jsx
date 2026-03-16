c3b7fd48540c48c1aae6ba82f08086aeab25cb5d
Saniya_Shete <shetesaniya22@gmail.com>
2026-03-16 18:03:28 +0530

    calculating number

---
+++ src/components/SumCalculator.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function SumCalculator() {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value);
+
+    if (!isNaN(value)) {
+      setNumbers([...numbers, value]);
+    }
+    e.target.value = "";
+  };
+
+  useEffect(() => {
+    const total = numbers.reduce((acc, num) => acc + num, 0);
+    setSum(total);
+  }, [numbers]);
+
+  return (
+    <>
+      <form>
+        <h1>Sum Calculator</h1>
+        <input type="number" name="number" id="number" onBlur={handleChange} />
+        <h2>Sum:{sum}</h2>
+      </form>
+    </>
+  );
+}
+
+export default SumCalculator;
